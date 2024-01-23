67a4b3d775c43b8c1be1cdb82d1079ca07539be4
Rudichev <125951437+Kl1mvill@users.noreply.github.com>
2024-01-23 18:54:14 +0400

    Update main.py
    Добавил комментарии

--- passwordManager/main.py
+++ passwordManager/main.py
@@ -2,13 +2,14 @@ from functions import *
 from login import *
 
 if __name__ == "__main__":
+	# Проверка наличия необходимых для работы файлов. Подробнее в login.py
 	if сhecking_files():
-		register()
+		register() # Регистрация пользователя
 
 	clear_console()
-	if login():
+	if login(): # Вход в приложение
 		clear_console()
-		while True:
+		while True: # Панель управления
 			print("1. Добавить и редактировать профиль",
 				  "2. Удалить профиль",
 				  "3. Посмотреть пароль",
@@ -17,16 +18,10 @@ if __name__ == "__main__":
 
 			command = input("Выберите действие >> ")
 			match command:
-				case "1":
-					add_profile()
-				case "2":
-					del_profile()
-				case "3":
-					view_profiles()
-				case "4":
-					generate_password()
-				case "5":
-					raise SystemExit
+				case "1": add_profile()
+				case "2": del_profile()
+				case "3": view_profiles()
+				case "4": generate_password()
+				case "5": raise SystemExit
 
-				case _:
-					print("Неизвестная команда"); clear_console()
+				case _: print("Неизвестная команда"); clear_console()
